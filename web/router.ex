39c3738c4b61d3302945593a81d129132b65bfe3
Bijan <bijanbwb@gmail.com>
2016-07-28 15:37:07 -0400

    Add /matches resources and run migration

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Pong.Router do
     pipe_through :browser # Use the default browser stack
 
     resources "/players", PlayerController
+    resources "/matches", MatchController
 
     get "/", PageController, :index
   end
