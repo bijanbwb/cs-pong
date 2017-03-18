cafc315c616ed704e6fe94a49dd792d3d34e6be9
AJ Foster <web@aj-foster.com>
2017-03-17 22:51:38 -0400

    Add fields necessary for Elo rankings

---
+++ priv/repo/migrations/20170318011058_add_elo_to_players.exs
@@ -0,0 +1,21 @@
+defmodule Pong.Repo.Migrations.AddEloToPlayers do
+  use Ecto.Migration
+
+  def up do
+    alter table(:players) do
+      add :games, :integer, null: false, default: 0
+      add :ranking, :float, null: false, default: 1000.0
+      add :provisional, :boolean, null: false, default: true
+    end
+    
+    create index(:players, [:ranking])
+  end
+  
+  def down do
+    alter table(:players) do
+      remove :games
+      remove :ranking
+      remove :provisional
+    end
+  end
+end
--- web/models/player.ex
+++ web/models/player.ex
@@ -4,6 +4,9 @@ defmodule Pong.Player do
   schema "players" do
     field :name, :string
     field :avatar_url, :string
+    field :games, :integer, default: 0
+    field :ranking, :float, default: 1000.0
+    field :provisional, :boolean, default: true
 
     timestamps()
   end
@@ -13,7 +16,15 @@ defmodule Pong.Player do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:name, :avatar_url])
+    |> cast(params, [:name, :avatar_url,])
     |> validate_required([:name])
+  end
+
+  @doc """
+  Builds a changeset for use by the rank calculator.
+  """
+  def rank_changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:games, :ranking, :provisional])
   end
 end
